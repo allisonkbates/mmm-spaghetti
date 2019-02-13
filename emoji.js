68534c5713aa25cf51a849a2d6a1d818540405de
Allison Bates <allisonkbates@gmail.com>
2019-02-12 22:40:18 -0500

    js updates

--- emoji.js
+++ emoji.js
@@ -4,33 +4,40 @@ window.onload = function () {
         var type = event.target.dataset.type;
         var multiple = event.target.dataset.multiple;
         var name = event.target.dataset.name;
-        var skin = document.querySelectorAll('div[data-type="skin"');
+        var skin = document.querySelectorAll('div[data-type="skin"]');
         var emojiFace = document.querySelector('.emoji-face');
-        var eyes = document.querySelector('div[data-type="eyes"');
-        var left = eyes.querySelector('.left');
-        console.log(left);
+        var eyes = document.querySelectorAll('div[data-type="eyes"]');
+        var left = document.querySelector('.left');
+        var right = document.querySelector('.right');
+        var mouth = document.querySelectorAll('div[data-type="mouth"]');
+        var emojiMouth = document.querySelector('.emoji-mouth');
 
         //list out all functions to use
         function removeSelected(element) {
             element.classList.remove('selected');
         }
 
-        function removeClasses(element) {
-            element.classList.remove('default','startled', 'heart', 'star', 'fire','squeeze','closed-up', 'closed-down', 'lined');
-        }
-
         //conditional logic
         if (event.target.classList.contains('js-choice')) {
-            if (type="skin") {
+            if (type === "skin") {
                 skin.forEach(removeSelected);
                 emojiFace.classList.add(name);
             }
-           if(type="eyes") {
+            
+            if(type === "eyes") {
                 eyes.forEach(removeSelected);
-                left.forEach(removeClasses);
+                right.classList.remove('default','startled', 'heart', 'star', 'fire','squeeze','closed-up', 'closed-down', 'lined');
+                left.classList.remove('default','startled', 'heart', 'star', 'fire','squeeze','closed-up', 'closed-down', 'lined');
+                right.classList.add(name);
                 left.classList.add(name);
             }
             
+            if (type === "mouth") {
+                mouth.forEach(removeSelected);
+                emojiMouth.classList.remove('default', 'no-mouth','upside', 'open', 'tongue', 'lined','frown','smile', 'curse', 'grimace');
+                emojiMouth.classList.add(name);
+            }
+
             event.target.classList.add('selected');
             
             //console.log(event.target.classList.add('selected'));
